f290596f198ed051dcd09696831eed513da8e97d
Yikang666 <2749436915@qq.com>
2023-10-15 00:21:00 +0800

    1

---
+++ Windows 10/shutdown/script.js
@@ -0,0 +1,12 @@
+var Button = document.getElementById("fullscreen");
+
+Button.addEventListener("click", function() {
+    document.documentElement.requestFullscreen();
+});
+
+function stop() {
+  return false;
+};
+
+document.oncontextmenu = stop;
+document.onselectstart = stop;
